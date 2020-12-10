162f4e38b60d17aa7ade2b4edd55a66feb9587b2
davidltk123 <davidltk123@gmail.com>
2020-12-10 20:35:25 +0800

    [Todo List SpringBoot][David] feat: implement create new todo request

--- src/main/java/com/thoughtworks/todolist/controllers/TodoController.java
+++ src/main/java/com/thoughtworks/todolist/controllers/TodoController.java
@@ -1,13 +1,12 @@
 package com.thoughtworks.todolist.controllers;
 
+import com.thoughtworks.todolist.dto.TodoRequest;
 import com.thoughtworks.todolist.dto.TodoResponse;
 import com.thoughtworks.todolist.mappers.TodoMapper;
 import com.thoughtworks.todolist.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,6 +27,12 @@ public class TodoController {
     @GetMapping("/{todoId}")
     public TodoResponse getById(@PathVariable String todoId){
         return todoMapper.toResponse(todoService.getById(todoId));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TodoResponse create(@RequestBody TodoRequest todoRequest){
+        return todoMapper.toResponse(todoService.create(todoMapper.toEntity(todoRequest)));
     }
 
 }
--- src/main/java/com/thoughtworks/todolist/services/TodoService.java
+++ src/main/java/com/thoughtworks/todolist/services/TodoService.java
@@ -22,6 +22,6 @@ public class TodoService {
     }
 
     public Todo create(Todo todo) {
-        return null;
+        return todoRepository.save(todo);
     }
 }
